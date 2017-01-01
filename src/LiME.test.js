4100d1ad44549b9fe81612aca077e46e2a244ba2
Samuel Littley <samuel.littley@toastwaffle.com>
2017-01-01 15:54:59 +0000

    LiME-ify (with logo!)

--- src/LiME.test.js
+++ src/LiME.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LiME from './LiME';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LiME />, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LiME from './LiME';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <LiME />,
   document.getElementById('root')
 );
