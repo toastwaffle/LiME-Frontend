8b7942f7df472a5858e163650c9d8649d725bf42
Samuel Littley <samuel.littley@toastwaffle.com>
2017-07-10 21:10:21 +0100

    Add I18n component

---
+++ src/components/I18n.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import i18n from '../i18n';
+
+function isString(thing) {
+  return (typeof thing === 'string' || thing instanceof String);
+}
+
+function replace(language, key) {
+  return (i18n[language || 'en_gb'] || {})[key] || key;
+}
+
+function maybeReplaceProp(language, props, propName) {
+  if (isString(props[propName])) {
+    props[propName] = replace(language, props[propName]);
+  }
+}
+
+class I18n extends React.Component {
+  render () {
+    const {component: Component, language: language, ...props} = this.props;
+
+    maybeReplaceProp(language, props, 'title');
+    maybeReplaceProp(language, props, 'children');
+
+    return (
+      <Component {...props} />
+    );
+  }
+}
+I18n.propTypes = {
+  component: PropTypes.any,
+  language: PropTypes.string,
+};
+I18n.defaultProps = {
+  component: 'span',
+};
+
+function mapStateToProps(state) {
+  return {
+    language: state.settings.language,
+  };
+}
+
+export default connect(mapStateToProps)(I18n);
