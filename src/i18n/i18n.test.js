8b967d94bcb2104356f0a7c28d128aa9bbac21f4
Samuel Littley <samuel.littley@toastwaffle.com>
2017-07-10 21:09:59 +0100

    Add tests for i18n data

---
+++ src/i18n/i18n.test.js
@@ -0,0 +1,18 @@
+import languages from '.';
+import keys from './keys';
+
+function languageTest(lang) {
+  test(lang + ' exists', () => {
+    expect(languages).toHaveProperty(lang);
+  });
+
+  test(lang + ' has required keys', () => {
+    for (const key of keys) expect(languages[lang]).toHaveProperty(key);
+  });
+
+  test(lang + ' has no unknown keys', () => {
+    for (const key in languages[lang]) expect(keys).toContain(key);
+  });
+}
+
+languageTest('en_gb');
